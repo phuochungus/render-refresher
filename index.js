6cfa89aec383ae9ba9642e0aac3e189443986db6
HungNP <21520252@gm.uit.edu.vn>
2023-12-08 19:38:45 +0700

    save change

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+require('dotenv').config()
+
+
+const urls = process.env.URLS.split(';') || [];
+console.log(urls)
+
+async function fetchURLS(urls) {
+    console.log("Fetching URLS")
+    const data = await Promise.all(urls.map(async (url) => {
+        await fetch(url);
+    }));
+    return data
+}
+
+app.get('/', async (req, res) => {
+    await fetchURLS(urls);
+    res.sendStatus(200);
+});
+
+app.get('/test', async (req, res) => {
+    console.log('hello')
+    res.sendStatus(200);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    setInterval(() => { fetchURLS(urls) }, 3 * 60 * 1000)
+});
