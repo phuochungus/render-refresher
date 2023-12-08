847505f08b3e68f49a6bfd682ee1076e9a0da0e0
HungNP <21520252@gm.uit.edu.vn>
2023-12-08 19:49:59 +0700

    change time

--- index.js
+++ index.js
@@ -7,19 +7,22 @@ const urls = process.env.URLS.split(';') || [];
 console.log(urls)
 
 async function fetchURLS(urls) {
-    console.log("Fetching URLS")
-    const data = await Promise.all(urls.map(async (url) => {
-        await fetch(url);
-    }));
-    return data
+    try {
+        for (url of urls) {
+            fetch(url);
+        }
+    } catch (error) {
+        console.error(error)
+    }
 }
 
 app.get('/test', async (req, res) => {
+    console.log("FETCHED!")
     res.sendStatus(200);
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-    setInterval(() => { fetchURLS(urls) }, 10 * 60 * 1000)
+    setInterval(() => { fetchURLS(urls) }, 5 * 60 * 1000)
 });
